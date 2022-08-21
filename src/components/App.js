34123bb6ab1a61427badf005cfbeba7b80370898
SaiSupraja <102989692+SaiSupraja18@users.noreply.github.com>
2022-08-21 20:07:38 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,27 @@
-import React from 'react'
-import '../styles/App.css';
+import React, {useEffect, useState} from 'react'
+import '../style/App.css';
 const App = () => {
-
+  const [time, setTime] = useState("12/17/2021, 6:24:00 PM");
+  const [intervalId, setIntervalId] = useState();
+  
+  useEffect(() => {
+    const newIntervalId = setInterval(() => {
+      setTime(new Date().toLocaleString());
+    }, 1000);
+    setInterval(newIntervalId);
+    
+    return () => {
+      clearInterval(intervalId);
+    }
+  }, []);
+  
   return (
     <div id="main">
-      <div className="date-time"></div>
-    </div>
-  )
+    <div className="date-time">
+    {time}
+</div>
+</div>
+}
 }
 
 
